cb027722592280fc0848b5c963d5f41d7250b9ef
Poloniumx86 <datgreenboi@protonmail.com>
2023-11-12 14:19:34 +0000

    final

--- main.py
+++ main.py
@@ -8,19 +8,6 @@ load_dotenv()
 
 openai.api_key = os.getenv("API_KEY")
 
-#style
-st.markdown(
-        f"""
-        <style>
-            body {{
-                background-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-photo%2Fpurple-grid-background_37222602.htm&psig=AOvVaw28xGz5OYq3jDIk_3gwMxeA&ust=1699879412304000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNjKgLa-voIDFQAAAAAdAAAAABAE');
-                background-size: cover;
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
 messages=[
     {"role": "system", "content": "We will provide you an image name, we want you to provide us meme text about sustainability that is related to the image name we provide. Keep the meme text short and be very funny. Don't mention the exact name of the image."},
   ]
